3152f0e32aaf7853cad7d1fa427a0a94234533c5
pettrz <petterknutsson5@gmail.com>
2018-02-02 16:53:27 +0100

    Added update function

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -42,6 +42,26 @@ module.exports = function(app, db){
     });
 
     //U=Update
+    app.put('/notes/:id', (req, res) => {
+
+        const myDB = db.db('notesdb');
+
+        const id = req.params.id;
+        const details = { '_id': new objectId(id) };
+        const note = { text: req.body.body, title: req.body.title };
+
+        myDB.collection('notes').update(details, note, (err, item) => {
+            if (err) {
+                res.send({ 'error': 'an error again...' });
+            } else {
+                res.send(note);
+            }
+        });
+
+        console.log('asking for a note');
+
+
+    }
 
     //D=Delete
 
