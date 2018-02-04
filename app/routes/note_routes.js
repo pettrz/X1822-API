5266c936e9ece6975a8686c6f3ecddfde93cb721
pettrz <petterknutsson5@gmail.com>
2018-02-04 20:41:47 +0100

    fixed server.js

--- app/routes/note_routes.js
+++ app/routes/note_routes.js
@@ -1,7 +1,7 @@
 var objectId = require('mongodb').ObjectId;
 var cors = require('cors');
 
-module.exports = function(app, db){
+module.exports = function (app, db) {
 
     //C=Create
     app.post('/notes', (req, res) => {
@@ -9,10 +9,15 @@ module.exports = function(app, db){
         const myDB = db.db('notesdb'); //namnet på db i mlab
         //myDB.collection('notes');
 
-        const note = { text: req.body.body, title: req.body.title };
-        myDB.collection('notes').insert(note, (err, result)=>{
-            if (err){
-                res.send({'error': 'An error has occurred'});
+        const note = {
+            text: req.body.body,
+            title: req.body.title
+        };
+        myDB.collection('notes').insert(note, (err, result) => {
+            if (err) {
+                res.send({
+                    'error': 'An error has occurred'
+                });
             } else {
                 res.send(result.ops[0]);
             }
@@ -24,23 +29,51 @@ module.exports = function(app, db){
 
     //R=Read
     app.get('/notes/:id', cors(), (req, res) => {
-        
+
         const myDB = db.db('notesdb');
 
         const id = req.params.id;
-        const details = {'_id' : new objectId(id)};
+        const details = {
+            '_id': new objectId(id)
+        };
         myDB.collection('notes').findOne(details, (err, item) => {
-            if (err){
-                res.send({'error': 'an erro again...'});
+            if (err) {
+                res.send({
+                    'error': 'an error again...'
+                });
             } else {
                 res.send(item);
             }
         });
-        
-        
+
+
         console.log('asking for a note');
         //res.send('this should be a returned note');
     });
+
+    app.get('/notes', cors(), (req, res) => {
+        const myDB = db.db('notesdb');
+
+        myDB.collection('notes').find({}).toArray((err, item) => {
+            if (err) {
+                res.send({
+                    'error': 'An error has occured'
+                });
+            } else {
+                res.send(item);
+            }
+        });
+
+        // myDB.collection('notes').find((err, item) => {
+        //     if(err) {
+        //         res.send({'error': 'An error has occured'});
+        //     }
+        //     else {
+        //         res.send(item);
+        //     }
+        // });
+        console.log('asking for all notes');
+    })
 
     //U=Update
     app.put('/notes/:id', cors(), (req, res) => {
@@ -48,18 +81,25 @@ module.exports = function(app, db){
         const myDB = db.db('notesdb');
 
         const id = req.params.id;
-        const details = { '_id': new objectId(id) };
-        const note = { text: req.body.body, title: req.body.title };
+        const details = {
+            '_id': new objectId(id)
+        };
+        const note = {
+            text: req.body.body,
+            title: req.body.title
+        };
 
-        myDB.collection('notes').update(details, note, (err, item) => {
+        myDB.collection('notes').update(details, note, (err, result) => {
             if (err) {
-                res.send({ 'error': 'an error again...' });
+                res.send({
+                    'error': 'an error again...'
+                });
             } else {
                 res.send(note);
             }
         });
 
-        console.log('asking for a note');
+        console.log('updating notes');
 
 
     });
@@ -70,15 +110,21 @@ module.exports = function(app, db){
         const myDB = db.db('notesdb');
 
         const id = req.params.id;
-        const details = { '_id': new objectId(id) };
+        const details = {
+            '_id': new objectId(id)
+        };
 
         myDB.collection('notes').remove(details, (err, item) => {
             if (err) {
-                res.send({ 'error': 'an error again...' });
+                res.send({
+                    'error': 'an error again...'
+                });
             } else {
                 res.send('Note' + id + 'was deleted.');
             }
         });
+
+        console.log('deleting notes');
     });
 
 
